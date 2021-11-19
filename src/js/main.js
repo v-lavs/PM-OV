9ad6ce58fddd55022a69e26f20ca212aa76a26fb
v-lavs <lavskavanda@gmail.com>
2021-11-19 17:03:08 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    var nav = $('.header__nav');
+
+    $('.btn-burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn-close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+    //HEADER SCROLL
+
+    function onHeaderScrol() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 40) {
+            jQuery(".header").addClass('header_active');
+        } else {
+            jQuery(".header").removeClass('header_active');
+        }
+    }
+
+    $(document).on('scroll', function () {
+        onHeaderScrol();
+    });
+
+    const home_banner_slider = new Swiper(".slider-services", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        speed: 1500,
+        navigation: {
+            nextEl: ".slider__arrow_next",
+            prevEl: ".slider__arrow_prev",
+        },
+    });
+
+});
