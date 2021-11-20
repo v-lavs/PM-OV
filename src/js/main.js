85b405381b88d2fe22844a8a9a6feb45b634f4ce
v-lavs <lavskavanda@gmail.com>
2021-11-20 19:39:55 +0200

    add video

--- src/js/main.js
+++ src/js/main.js
@@ -145,59 +145,60 @@ $(document).ready(function () {
         $(totalIndexEL).html(this.slides.length);
     });
 
-    //    VIDEO YOUTYBE
-    // function findVideos() {
-    //     let videos = document.querySelectorAll('.video__content');
-    //
-    //     for (let i = 0; i < videos.length; i++) {
-    //         setupVideo(videos[i]);
-    //     }
-    // }
-    //
-    // function setupVideo(video) {
-    //     let link = video.querySelector('.video__link');
-    //     let media = video.querySelector('.video__media');
-    //     let button = video.querySelector('.btn_video');
-    //     // let id = parseMediaURL(media);
-    //
-    //     let id = media.getAttribute('data-video');
-    //
-    //     video.addEventListener('click', () => {
-    //         let iframe = createIframe(id);
-    //
-    //         link.remove();
-    //         button.remove();
-    //         video.appendChild(iframe);
-    //     });
-    //
-    //     link.removeAttribute('href');
-    //     video.classList.add('video_enabled');
-    // }
-    //
-    // // function parseMediaURL(media) {
-    // //     let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-    // //     let url = media.src;
-    // //     let match = url.match(regexp);
-    // //
-    // //     return match[1];
-    // // }
-    //
-    // function createIframe(id) {
-    //     let iframe = document.createElement('iframe');
-    //
-    //     iframe.setAttribute('allowfullscreen', '');
-    //     iframe.setAttribute('allow', 'autoplay');
-    //     iframe.setAttribute('src', generateURL(id));
-    //     iframe.classList.add('video__media');
-    //
-    //     return iframe;
-    // }
-    //
-    // function generateURL(id) {
-    //     let query = '?rel=0&showinfo=0&autoplay=1';
-    //     return 'https://www.youtube.com/embed/' + id + query;
-    // }
-    //
-    // findVideos();
 
+//    VIDEO YOUTYBE
+    function findVideos() {
+        let videos = document.querySelectorAll('.video__content');
+
+        for (let i = 0; i < videos.length; i++) {
+            setupVideo(videos[i]);
+        }
+    }
+
+    function setupVideo(video) {
+        let link = video.querySelector('.video__link');
+        let media = video.querySelector('.video__media');
+        let button = video.querySelector('.btn_video');
+        // let id = parseMediaURL(media);
+
+        let id = media.getAttribute('data-video');
+
+        video.addEventListener('click', () => {
+            let iframe = createIframe(id);
+
+            link.remove();
+            button.remove();
+            video.appendChild(iframe);
+        });
+
+        link.removeAttribute('href');
+        video.classList.add('video_enabled');
+    }
+
+    // function parseMediaURL(media) {
+    //     let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+    //     let url = media.src;
+    //     let match = url.match(regexp);
+    //
+    //     return match[1];
+    // }
+
+    function createIframe(id) {
+        let iframe = document.createElement('iframe');
+
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('allow', 'autoplay');
+        iframe.setAttribute('src', generateURL(id));
+        iframe.classList.add('video__media');
+
+        return iframe;
+    }
+
+    function generateURL(id) {
+        let query = '?rel=0&showinfo=0&autoplay=1';
+
+        return 'https://www.youtube.com/embed/' + id + query;
+    }
+
+    findVideos();
 });
